081bd374e14df850ff94633d59e38f54a112d078
Akhil Mohan <akhilerm@gmail.com>
2015-11-29 23:22:55 +0530

    Crash in Android 6.0 resolved
    Crash in Android 6.0 resolved. Haven't checked the code on API <23

--- app/src/main/java/com/arjunsajeev/netfast/MainActivity.java
+++ app/src/main/java/com/arjunsajeev/netfast/MainActivity.java
@@ -1,5 +1,6 @@
 package com.arjunsajeev.netfast;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.PendingIntent;
 import android.app.ProgressDialog;
@@ -7,8 +8,11 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.SmsManager;
 import android.view.View;
@@ -22,26 +26,27 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    final int MY_PERMISSIONS_REQUEST_SEND_SMS=20;
+    Spinner planSpinner;
+    ProgressDialog[] progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        final Spinner planSpinner = (Spinner) findViewById(R.id.spinner);
         Button activateButton = (Button)findViewById(R.id.activateButton);
-        final ProgressDialog[] progressDialog = new ProgressDialog[1];
         final TextView planTextView = (TextView)findViewById(R.id.textView);
+        planSpinner=(Spinner)findViewById(R.id.spinner);
+        progressDialog = new ProgressDialog[1];
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.plans_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         planSpinner.setAdapter(adapter);
-
         planSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 String plan = planSpinner.getSelectedItem().toString();
                 //plan = "R.string." + plan;
-               // planTextView.setText(getApplicationContext().getResources().getString(Integer.parseInt(plan)));
+                // planTextView.setText(getApplicationContext().getResources().getString(Integer.parseInt(plan)));
                 //planTextView.setText("Data Plan Selected!"+Math.random());
                 String[] planDescriptions = getApplicationContext().getResources().getStringArray(R.array.plan_descriptions);
                 planTextView.setText(planDescriptions[position]);
@@ -57,11 +62,26 @@ public class MainActivity extends AppCompatActivity {
         activateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               progressDialog[0] = ProgressDialog.show(MainActivity.this, "Status",
-                        "Sending Activation SMS", true);
-                String plan=  planSpinner.getSelectedItem().toString();
-                //Toast.makeText(getApplicationContext(),plan,Toast.LENGTH_LONG).show();
-                String phoneNumber = "123";
+                if (ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.SEND_SMS)
+                        != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(MainActivity.this,
+                                new String[]{Manifest.permission.SEND_SMS},
+                                MY_PERMISSIONS_REQUEST_SEND_SMS);
+                }
+                else
+                    SMS();
+            }
+        });
+    }
+
+ //SMS function to send the SMS
+    void SMS(){
+        progressDialog[0] = ProgressDialog.show(MainActivity.this, "Status",
+                "Sending Activation SMS", true);
+        String plan = planSpinner.getSelectedItem().toString();
+        //Toast.makeText(getApplicationContext(),plan,Toast.LENGTH_LONG).show();
+        String phoneNumber = "123";
 
 
 //                Uri uri = Uri.parse("smsto:" + phoneNumber);
@@ -73,61 +93,79 @@ public class MainActivity extends AppCompatActivity {
 //
 //                startActivity(smsIntent);
 
-                String smsBody = "STV "+plan;
+        String smsBody = "STV " + plan;
 
-                String SMS_SENT = "SMS_SENT";
-                String SMS_DELIVERED = "SMS_DELIVERED";
+        String SMS_SENT = "SMS_SENT";
+        String SMS_DELIVERED = "SMS_DELIVERED";
 
-                PendingIntent sentPendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, new Intent(SMS_SENT), 0);
-                PendingIntent deliveredPendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, new Intent(SMS_DELIVERED), 0);
+        PendingIntent sentPendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, new Intent(SMS_SENT), 0);
+        PendingIntent deliveredPendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, new Intent(SMS_DELIVERED), 0);
 
 // For when the SMS has been sent
-                registerReceiver(new BroadcastReceiver() {
-                    @Override
-                    public void onReceive(Context context, Intent intent) {
-                        switch (getResultCode()) {
-                            case Activity.RESULT_OK:
-                                progressDialog[0].dismiss();;
-                                Toast.makeText(context, "Activation SMS sent successfully", Toast.LENGTH_SHORT).show();
-                                break;
-                            case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
-                                Toast.makeText(context, "Generic failure cause", Toast.LENGTH_SHORT).show();
-                                break;
-                            case SmsManager.RESULT_ERROR_NO_SERVICE:
-                                Toast.makeText(context, "Service is currently unavailable", Toast.LENGTH_SHORT).show();
-                                break;
-                            case SmsManager.RESULT_ERROR_NULL_PDU:
-                                Toast.makeText(context, "No pdu provided", Toast.LENGTH_SHORT).show();
-                                break;
-                            case SmsManager.RESULT_ERROR_RADIO_OFF:
-                                Toast.makeText(context, "Radio was explicitly turned off", Toast.LENGTH_SHORT).show();
-                                break;
-                        }
-                    }
-                }, new IntentFilter(SMS_SENT));
+        registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                switch (getResultCode()) {
+                    case Activity.RESULT_OK:
+                        progressDialog[0].dismiss();
+                        Toast.makeText(context, "Activation SMS sent successfully", Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                        Toast.makeText(context, "Generic failure cause", Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NO_SERVICE:
+                        Toast.makeText(context, "Service is currently unavailable", Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NULL_PDU:
+                        Toast.makeText(context, "No pdu provided", Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_RADIO_OFF:
+                        Toast.makeText(context, "Radio was explicitly turned off", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        }, new IntentFilter(SMS_SENT));
 
 // For when the SMS has been delivered
-                registerReceiver(new BroadcastReceiver() {
-                    @Override
-                    public void onReceive(Context context, Intent intent) {
-                        switch (getResultCode()) {
-                            case Activity.RESULT_OK:
-                                Toast.makeText(getBaseContext(), "Activation SMS delivered", Toast.LENGTH_SHORT).show();
-                                break;
-                            case Activity.RESULT_CANCELED:
-                                Toast.makeText(getBaseContext(), "Activation SMS not delivered", Toast.LENGTH_SHORT).show();
-                                break;
-                        }
-                    }
-                }, new IntentFilter(SMS_DELIVERED));
+        registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                switch (getResultCode()) {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getBaseContext(), "Activation SMS delivered", Toast.LENGTH_SHORT).show();
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Toast.makeText(getBaseContext(), "Activation SMS not delivered", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        }, new IntentFilter(SMS_DELIVERED));
 
 // Get the default instance of SmsManager
-                SmsManager smsManager = SmsManager.getDefault();
+        SmsManager smsManager = SmsManager.getDefault();
 // Send a text based SMS
-                smsManager.sendTextMessage(phoneNumber, null, smsBody, sentPendingIntent, deliveredPendingIntent);
+        smsManager.sendTextMessage(phoneNumber, null, smsBody, sentPendingIntent, deliveredPendingIntent);
 
+    }
+
+//Handle the requestPermissions call
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_SEND_SMS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //Permission Granted
+                        SMS();
+                } else {
+                    //Permission Denied
+                    Toast.makeText(MainActivity.this, "App does not have enough permissions", Toast.LENGTH_SHORT).show();
+                }
             }
-        });
-
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 }
